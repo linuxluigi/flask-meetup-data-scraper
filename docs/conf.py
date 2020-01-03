504123a219226d85826d18ff31401f006f6e49a8
Steffen Exler <steffen.exler@pm.me>
2020-01-03 10:50:45 +0100

    add sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ from typing import List
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration -----------------------------------------------------
 
@@ -25,7 +26,11 @@ from typing import List
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = List[str]
+extensions: List[str] = [
+    "sphinx.ext.autodoc",
+]
+
+autoclass_content = "both"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
