85d5e831f72e460c0420bc981c8b2d73e8ba522f
Steffen Exler <steffen.exler@pm.me>
2020-01-20 09:24:23 +0100

    add sandbox test case

--- tests/commands/test_get_group.py
+++ tests/commands/test_get_group.py
@@ -15,10 +15,14 @@ def test_get_group(meetup_groups: dict, app: Flask):
     result_2: Result = runner.invoke(get_group, [meetup_groups["sandbox"]["urlname"]])
     assert result_2.exit_code == 0
 
+    # test with sandbox group
+    result_3: Result = runner.invoke(get_group, ["--sandbox", "True"])
+    assert result_3.exit_code == 0
+
     # test with not-exist group
-    result_3: Result = runner.invoke(get_group, [meetup_groups["not-exist"]["urlname"]])
-    assert result_3.exit_code == 2
+    result_4: Result = runner.invoke(get_group, [meetup_groups["not-exist"]["urlname"]])
+    assert result_4.exit_code == 2
 
     # test with gone group
-    result_4: Result = runner.invoke(get_group, [meetup_groups["gone"]["urlname"]])
-    assert result_4.exit_code == 2
+    result_5: Result = runner.invoke(get_group, [meetup_groups["gone"]["urlname"]])
+    assert result_5.exit_code == 2
