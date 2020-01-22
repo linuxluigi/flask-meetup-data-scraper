c6963e9912cedffa1acd2ac8a29b0af7c0210313
Steffen Exler <steffen.exler@pm.me>
2020-01-22 16:55:11 +0100

    update treafik to 2.x

--- config/production.py
+++ config/production.py
@@ -1,10 +1,8 @@
-from config.base import *
 from envparse import env
+
+from config.base import *
 
 # Flask
 # ------------------------------------------------------------------------------
 TESTING = False
 DEBUG = False
-
-# Server Name https://flask.palletsprojects.com/en/1.1.x/config/#SERVER_NAME
-SERVER_NAME = env("SERVER_NAME", default="127.0.0.1")
