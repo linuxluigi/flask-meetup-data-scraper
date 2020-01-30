9480dff6857e7f3d66a2f075f27662d26dfd7530
Steffen Exler <steffen.exler@pm.me>
2020-01-30 10:24:11 +0100

    add workaround command reset_index

--- meetup_search/commands/reset_index.py
+++ meetup_search/commands/reset_index.py
@@ -2,11 +2,11 @@ from time import sleep
 from typing import List
 
 import click
+from elasticsearch import Elasticsearch
 from environs import Env
 from flask.cli import with_appcontext
 from flask.config import Config
 
-import app
 from meetup_search.commands.load_groups import load_groups
 from meetup_search.commands.load_zip_codes import load_zip_codes
 from meetup_search.commands.migrate_models import migrate_models
@@ -34,14 +34,16 @@ def reset_index(waring_time: int):
         print("You stop this action, nothing changed...")
         return
 
-    # load app config
-    app_config: Config = app.create_app().config
-
     print("The Elasticsearch index will now be delted ...")
 
     # delete group & meetup zip index
-    app_config["ES"].indices.delete(index=Group.Index.name, ignore=[400, 404])
-    app_config["ES"].indices.delete(index=MeetupZip.Index.name, ignore=[400, 404])
+    # fixme load es from flask base config!
+    env: Env = Env()
+    elasticsearch: Elasticsearch = Elasticsearch(
+        [{"host": env("http.host"), "port": env("http.port")}]
+    )
+    elasticsearch.indices.delete(index=Group.Index.name, ignore=[400, 404])
+    elasticsearch.indices.delete(index=MeetupZip.Index.name, ignore=[400, 404])
 
     sleep(2)
 
